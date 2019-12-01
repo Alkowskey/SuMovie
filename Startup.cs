0bf4da9d4618658c77c39313b82999ee362b9f7c
Alkowskey <Alkowskey@gmail.com>
2019-12-01 15:17:51 +0100

    Basic scrapping - imdb

--- Startup.cs
+++ Startup.cs
@@ -2,6 +2,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+
+using Npgsql.EntityFrameworkCore.PostgreSQL;
+
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -10,6 +13,11 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+using SuMovie.Tools;
+using SuMovie.Context;
 
 namespace SuMovie
 {
@@ -25,7 +33,14 @@ namespace SuMovie
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<ImdbDbContext>(options =>
+            {
+                options.UseNpgsql(Configuration.GetConnectionString("imdbprojectConnection"));
+            });
+            services.AddHttpClient();
+            services.AddAngleSharp();
             services.AddControllers();
+            services.AddSingleton<ImdbScraper>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
